512ab43e102d6807ddb60fe9047dcbc90a24eff9
orazbek <oraz.daurenov@gmail.com>
2025-01-17 00:04:46 +0100

    WIP
    Co-authored-by: TheMcnafaha <TheMcnafaha@users.noreply.github.com>

--- src/app/components/RenderData.tsx
+++ src/app/components/RenderData.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useState } from "react";
-import { Items } from "../api-services/Data";
+import Data, { Items } from "../api-services/Data";
 import Breakfast from "./Breakfast";
 import ListItem from "./ListItem";
 import Lunch from "./Lunch";
@@ -14,8 +14,24 @@ type RenderDataProps = {
 };
 export default function RenderData(props: RenderDataProps) {
   const [items, setItems] = useState(props.allData);
+
+  const categories = props.allData.map((e) => e.category);
+  const searchInput = "Sn";
+
+  const result: string[] = [];
+  // go through every element of the array
+  // if the current element has the search string, add it to the result array
+  for (let index = 0; index < categories.length; index++) {
+    const element = categories[index];
+    if (element?.includes(searchInput)) {
+      result.push(element);
+    }
+  }
+  console.log("results: ", result);
+
   return (
     <>
+      <InputFilter />
       <Breakfast handleClick={setItems} />
       <Lunch handleClick={setItems} />
       <AllMenu handleClick={setItems} />
@@ -27,3 +43,27 @@ export default function RenderData(props: RenderDataProps) {
     </>
   );
 }
+
+const InputFilter = () => {
+  const [filter, setFilter] = useState("");
+  const searchingResult = Data;
+  const result = [];
+  for (let index = 0; index > searchingResult.length; index ++) {
+    const element = searchingResult[index];
+    if ()
+  }
+  return (
+    <>
+      <label htmlFor="">Type here:</label>
+      <input
+        className="m-3 bg-slate-600 text-white"
+        type="text"
+        name="filter"
+        id="filter"
+        value={filter}
+        onChange={(event) => setFilter(event.target.value)}
+      />
+      <p>{filter}</p>
+    </>
+  );
+};
